465c58bc92179de89e747dfa09dcb6960da0a683
prasannap <prasanna.patankar@synerzip.com>
2018-01-30 17:45:41 +0530

    Dashboard channges

--- ASPCore/ClientApp/components/Dashboard.tsx
+++ ASPCore/ClientApp/components/Dashboard.tsx
@@ -1,10 +1,11 @@
-﻿import * as React from 'react';
+﻿/// <reference path="counter.tsx" />
+import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 import 'isomorphic-fetch';
 import * as JQuery from 'jquery';
 import * as BT from 'bootstrap';
 import { Link, NavLink, withRouter, BrowserRouter } from 'react-router-dom';
-
+import { Child } from './Child';
 
 //import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'bootstrap';
 
@@ -12,6 +13,7 @@ import { Link, NavLink, withRouter, BrowserRouter } from 'react-router-dom';
 interface DashboardExampleState {
     Matches: MatchDetails[];
     loading: boolean;
+    PScore: any;
 }
 
 export class Dashboard extends React.Component<RouteComponentProps<{}>, DashboardExampleState> {
@@ -19,7 +21,7 @@ export class Dashboard extends React.Component<RouteComponentProps<{}>, Dashboar
     constructor() {
         super();
         //this.handleClick = this.handleClick.bind(this);
-        this.state = { Matches: [], loading: true };
+        this.state = { Matches: [], loading: true, PScore: null };
 
         fetch('http://172.25.29.70:92/API/Cricket/GetCurrentMatches')
             .then(response => response.json() as Promise<MatchDetails[]>)
@@ -44,9 +46,29 @@ export class Dashboard extends React.Component<RouteComponentProps<{}>, Dashboar
         </div>;
     }
 
+    //public static test(Mname: string): PlayerScore {
+    //    //return Mname + "test";
+    //    let PS: PlayerScore = {
+    //        MatchId: "",
+    //        Name: "",
+    //        Runs: "",
+    //        Wickets: ""
 
+    //    };
+
+    //    fetch('http://172.25.29.70:92/API/Cricket/Mashrafe%20Mortaza/18738/GetPlayerScore')
+    //        .then(response => response.json() as Promise<PlayerScore>)
+    //        .then(data => {
+    //            PS = data as PlayerScore;    
+    //            //console.log(PS);
+    //        });  
+    //    console.log(PS);
+    //    return PS;
+    //}
     private static renderMatchCard(Matches: MatchDetails[]) {
+        var PS: PlayerScore;
         return <div className='container'>
+
             <div className='row'>
                 {Matches.map(Match =>
                     <div className='col-lg-3' style={{ padding: '1px' }} key={Match.URL}>
@@ -58,13 +80,12 @@ export class Dashboard extends React.Component<RouteComponentProps<{}>, Dashboar
                                 <h4>{Match.Status}</h4>
                                 <hr />
                                 <p className='card-text'>{Match.Details}</p>
-
                             </div>
                             <div className='card-footer text-muted' style={{}} >
                                 <div className='row' style={{ paddingLeft: 22 }}>
-                                    <Link to={"/PlayerList?mid=" + Match.URL + "&mname=" + Match.Name} className="'btn btn-info btn-lg" disabled={Match.Status != 'Match will start soon.' ? false : true}>Teams</Link>&nbsp;
-                                    <Link to={"/Scoreboard?mid=" + Match.URL + "&mname=" + Match.Name} className="btn btn-success btn-lg" disabled={Match.Status != 'Match will start soon.' ? false : true}>Score</Link>&nbsp;
-                                     <Link to={"/Scoreboard?mid=" + Match.URL + "&mname=" + Match.Name} className="btn btn-outline-warning btn-lg" disabled={Match.Status != 'Match will start soon.' ? false : true}>Bet</Link>
+                                    <Link to={"/PlayerList?mid=" + Match.URL + "&mname=" + Match.Name} className='btn btn-info btn-lg' disabled={Match.Status != 'Match will start soon.' ? false : true}>Teams</Link>&nbsp;
+                                    <Link to={"/Scoreboard?mid=" + Match.URL + "&mname=" + Match.Name} className='btn btn-success btn-lg' disabled={Match.Status != 'Match will start soon.' ? false : true}>Score</Link>&nbsp;
+                                    <Link to={"/Scoreboard?mid=" + Match.URL + "&mname=" + Match.Name} className='btn btn-outline-warning btn-lg' disabled={Match.Status != 'Match will start soon.' ? false : true}>Bet</Link>
 
                                 </div>
                             </div>
@@ -82,4 +103,11 @@ interface MatchDetails {
     Name: string;
     Status: string;
     URL: string;
+}
+
+interface PlayerScore {
+    MatchId: string;
+    Name: string;
+    Runs: string;
+    Wickets: string;
 }
--- ASPCore/ClientApp/routes.tsx
+++ ASPCore/ClientApp/routes.tsx
@@ -7,6 +7,7 @@ import { Counter } from './components/Counter';
 import { Dashboard } from './components/Dashboard';
 import { Scoreboard } from './components/Scoreboard';
 import { PlayerList } from './components/PlayerList';
+import { Child } from './components/Child';
 
 export const routes = <Layout>
     <Route exact path='/' component={Home} />
@@ -16,4 +17,5 @@ export const routes = <Layout>
     </Route>
     <Route path='/Scoreboard' component={Scoreboard} />
     <Route path='/PlayerList' component={PlayerList} />
+    <Route path='/Child' component={Child} />
 </Layout>;
