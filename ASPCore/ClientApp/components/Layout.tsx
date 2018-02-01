fae6af0e8a7ecf694d46789a044f56c9f8ff7526
prasannap <prasanna.patankar@synerzip.com>
2018-02-01 12:25:59 +0530

    Revert "test 2"
    This reverts commit f37cc2aeed067f923b67b23420e1ce7f76775c9e.

--- ASPCore/ClientApp/components/Layout.tsx
+++ ASPCore/ClientApp/components/Layout.tsx
@@ -5,17 +5,58 @@ export interface LayoutProps {
     children?: React.ReactNode;
 }
 
-export class Layout extends React.Component<LayoutProps, {}> {
+interface LoginState {
+    loggedin: boolean;
+}
+
+export class Layout extends React.Component<LayoutProps, LoginState> {
+    constructor() {
+        super();
+        this.state = { loggedin: false };
+    }
     public render() {
         return <div className='container-fluid'>
             <div className='row'>
-                <div className='col-sm-3'>
-                    <NavMenu />
+                <div style={{ visibility: this.state.loggedin ? 'visible' : 'hidden' }}>
+                    <div className='col-sm-3'>
+                        <NavMenu />
+                    </div>
+                    <div className='col-sm-9'>
+                        {this.props.children}
+                    </div>
                 </div>
-                <div className='col-sm-9'>
-                    {this.props.children}
+                <div style={{ visibility: this.state.loggedin ? 'hidden' : 'visible' }}>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-5">
+                                <div className="panel panel-default" style={{ margin: 30, height: 300, width: 500 }}>
+                                    <table>
+                                        <tr >
+                                            <label style={{ fontSize: 20, margin: 20 }}>UserName</label>
+                                            <input type="text" width='20' style={{ fontSize: 15 }} />
+                                        </tr>
+                                        <tr >
+                                            <label style={{ fontSize: 20, margin: 20 }}>Password</label>
+                                            <input type="text" width='20' style={{ fontSize: 15 }} />
+                                        </tr>
+                                        <tr >
+                                            <button onClick={() => { this.incrementCounter() }}>Login</button>
+                                        </tr>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
         </div>;
     }
+
+    incrementCounter() {
+        this.setState({
+            loggedin: true
+        });
+    }
 }
+
--- ASPCore/ClientApp/routes.tsx
+++ ASPCore/ClientApp/routes.tsx
@@ -1,33 +1,20 @@
 import * as React from 'react';
 import { Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
-import { Betting } from './components/Betting';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 import { Dashboard } from './components/Dashboard';
 import { Scoreboard } from './components/Scoreboard';
 import { PlayerList } from './components/PlayerList';
-<<<<<<< HEAD
 import { Login } from './components/Login';
-=======
-import { Login } from './components/login';
-import { Register } from './components/Register';
->>>>>>> a13ae0758eed063f1cab155e3f3040b0ed3e90e4
 
 export const routes = <Layout>
     <Route exact path='/Home' component={Home} />
     <Route path='/counter' component={Counter} />
     <Route path='/fetchdata' component={FetchData} />
-<<<<<<< HEAD
     <Route path='/' component={Dashboard} >        
     </Route>
     <Route path='/Scoreboard' component={Scoreboard} />
     <Route path='/PlayerList' component={PlayerList} />
-=======
-    <Route path='/Dashboard' component={Dashboard} />
-    <Route path='/Scoreboard' component={Scoreboard} />
-    <Route path='/PlayerList' component={PlayerList} />
-    <Route path='/Register' component={Register} />
->>>>>>> a13ae0758eed063f1cab155e3f3040b0ed3e90e4
 </Layout>;
